4abbebaef66cf9a4b109642296c3e3138f1128ec
vinny <vinnyroyal1@gmail.com>
2024-09-06 19:53:56 +0530

    modified Multiply.java file

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,6 @@ public class Multiply {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int c = a*b;
-        System.out.println(c);
+        System.out.println(a*b);
     }
 }
